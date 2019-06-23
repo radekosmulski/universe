f08857862f739177cf57a2450ff855654440f040
Radek Osmulski <rosmulski@gmail.com>
2019-06-23 17:20:50 +0200

    make relative paths absolute

--- config_scripts/basic_agent_forwarding_required.sh
+++ config_scripts/basic_agent_forwarding_required.sh
@@ -1,5 +1,5 @@
 # Sync dot files
-rm .bashrc .vimrc
+rm ~/.bashrc ~/.vimrc
 git clone --bare git@github.com:radekosmulski/dotfiles.git $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout headless
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
