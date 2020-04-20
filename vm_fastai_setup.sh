496367ed66bf02f3fc296008e73e7c1233e36cc5
Radek Osmulski <rosmulski@gmail.com>
2020-04-20 07:16:31 +0200

    add vm_fastai_setup.sh

---
+++ vm_fastai_setup.sh
@@ -0,0 +1,29 @@
+# starting with a fastai pytorch image on GCP
+
+jupyter notebook --generate-config
+echo "c.NotebookApp.token = ''" >> ~/.jupyter/jupyter_notebook_config.py
+
+mkdir workspace
+cd workspace
+
+git clone https://github.com/fastai/nbdev
+pip install -e nbdev
+
+git clone https://github.com/fastai/fastcore
+cd fastcore
+pip install -e ".[dev]"
+cd ..
+
+git clone https://github.com/fastai/fastai2
+cd fastai2
+conda env create -f environment.yml
+pip install -e ".[dev]"
+source activate fastai2
+cd ..
+
+curl https://rclone.org/install.sh | sudo bash
+
+mkdir setup_scripts && cd setup_scripts
+wget https://raw.githubusercontent.com/radekosmulski/universe/master/config_scripts/basic.sh && bash basic.sh
+wget https://raw.githubusercontent.com/radekosmulski/universe/master/config_scripts/basic_agent_forwarding_required.sh && bash basic_agent_forwarding_required.sh
+cd ..
