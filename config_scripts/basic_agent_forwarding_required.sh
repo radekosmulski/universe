1a01da92d05a793bbeeb0a791d9516dab31e5c3c
Radek Osmulski <rosmulski@gmail.com>
2023-07-09 16:08:41 +1000

    Update basic_agent_forwarding_required.sh

--- config_scripts/basic_agent_forwarding_required.sh
+++ config_scripts/basic_agent_forwarding_required.sh
@@ -1,5 +1,7 @@
 # Sync dot files
-rm ~/.bashrc ~/.vimrc
+mv ~/.bashrc ~/.bashrc.old
+mv ~/.vimrc ~/.vimrc.old
+#rm ~/.bashrc ~/.vimrc
 git clone --bare git@github.com:radekosmulski/dotfiles.git $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout headless
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
